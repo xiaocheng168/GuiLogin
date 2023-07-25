005a2750da5c78bc523c47f31b30a46f40f7af96
Zcc <x3513312668@gmail.com>
2023-07-25 19:15:19 +0800

    添加了重载命令方法

--- src/main/java/cc/mcyx/AuthLogin.java
+++ src/main/java/cc/mcyx/AuthLogin.java
@@ -1,13 +1,35 @@
 package cc.mcyx;
 
-import cc.mcyx.listener.GuiListener;
+import cc.mcyx.config.MessageConfig;
+import cc.mcyx.listener.ChatListener;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public class AuthLogin extends JavaPlugin {
+
+    public static AuthLogin authLogin;
+
+    @Override
+    public void onLoad() {
+        authLogin = this;
+        this.saveDefaultConfig();
+    }
+
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new GuiListener(), this);
+        Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
+//        Bukkit.getPluginManager().registerEvents(new GuiListener(), this);
         getLogger().info("GuiAuth 已载入");
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        this.reloadConfig();
+        sender.sendMessage(Component.text(MessageConfig.getMessage("reload", "配置文件已刷新!")));
+        return true;
     }
 }
