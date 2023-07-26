58a3208d647681e3c8e56b60a0a0ebda753d6e09
Zcc <x3513312668@gmail.com>
2023-07-26 15:28:28 +0800

    聊天登录方式

--- src/main/java/cc/mcyx/listener/ChatNetworkListener.java
+++ src/main/java/cc/mcyx/listener/ChatNetworkListener.java
@@ -1,25 +1,25 @@
 package cc.mcyx.listener;
 
-import cc.mcyx.config.MessageConfig;
+import cc.mcyx.config.ConfigManager;
+import cc.mcyx.config.MessageSender;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import fr.xephi.authme.api.v3.AuthMeApi;
-import net.kyori.adventure.text.Component;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-//数据数据包监听
-public class NetworkListener extends PacketAdapter {
+//聊天快速登录
+public class ChatNetworkListener extends PacketAdapter {
     public static AuthMeApi authMeApi = AuthMeApi.getInstance();
 
-    public NetworkListener(Plugin plugin, PacketType... types) {
+    public ChatNetworkListener(Plugin plugin, PacketType... types) {
         super(plugin, types);
     }
 
     @Override
     public void onPacketReceiving(PacketEvent event) {
+        if (!ConfigManager.getSetting("type", "gui").toString().equalsIgnoreCase("chat")) return;
         Player player = event.getPlayer();
         try {
             //用户没有登录的情况下
@@ -32,24 +32,23 @@ public class NetworkListener extends PacketAdapter {
                 //用户是否已注册
                 if (!authMeApi.isRegistered(playerName)) {
                     //注册用户
-                    sendMessage(player, authMeApi.registerPlayer(playerName, password) ? MessageConfig.getMessage("register.success", "注册成功") : MessageConfig.getMessage("register.error", "登录失败,可能是账号或者密码错误"));
+                    MessageSender.sendMessage(player, authMeApi.registerPlayer(playerName, password) ? ConfigManager.getMessage("register.success", "注册成功") : ConfigManager.getMessage("register.error", "登录失败,可能是账号或者密码错误"));
+                    //注册完后是否自动登录
+                    if ((Boolean) (ConfigManager.getSetting("register_auto_login"))) authMeApi.forceLogin(player);
                 } else {
                     //判断是否登录成功 成功将听过验证 否则失败！
                     if (authMeApi.checkPassword(playerName, password)) {
                         authMeApi.forceLogin(player);
-                        sendMessage(player, MessageConfig.getMessage("login.success", "登录成功"));
-
-                    } else sendMessage(player, MessageConfig.getMessage("login.error", "登录失败，可能是密码不正确哦"));
+                        MessageSender.sendMessage(player, ConfigManager.getMessage("login.success", "登录成功"));
+                    } else
+                        MessageSender.sendMessage(player, ConfigManager.getMessage("login.error", "登录失败，可能是密码不正确哦"));
                 }
             }
         } catch (Exception e) {
-            sendMessage(player, "§4服务器登录校验错误!请联系服务器内管理员! 异常点: " + e.getMessage());
+            MessageSender.sendMessage(player, "§4服务器登录校验错误!请联系服务器内管理员! 异常点: " + e.getMessage());
             e.printStackTrace();
         }
 
     }
 
-    public void sendMessage(CommandSender sender, String msg) {
-        sender.sendMessage(Component.text(msg.replace("&", "§")));
-    }
 }
