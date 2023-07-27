d56775f13f01190feeac0226f098dbb9b4d3d712
Zcc <x3513312668@gmail.com>
2023-07-27 11:30:27 +0800

    加入了统计数据API bStats

--- src/main/java/cc/mcyx/FastAuth.java
+++ src/main/java/cc/mcyx/FastAuth.java
@@ -1,5 +1,6 @@
 package cc.mcyx;
 
+import cc.mcyx.api.Metrics;
 import cc.mcyx.config.ConfigManager;
 import cc.mcyx.listener.GuiNetworkListener;
 import cc.mcyx.listener.ChatNetworkListener;
@@ -19,12 +20,21 @@ public class FastAuth extends JavaPlugin {
     public void onLoad() {
         fastAuth = this;
         this.saveDefaultConfig();
+        try {
+            //统计数据
+            Metrics metrics = new Metrics(this, 19260);
+            metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "Fast Auth!!!!~~ 轻、快、用"));
+        } catch (Exception e) {
+            getLogger().warning("统计数据API异常!" + e.getLocalizedMessage());
+        }
+
     }
 
     @Override
     public void onEnable() {
         getLogger().info("只为登录更快一步!");
         getLogger().info("FastAuth 已载入");
+        getLogger().info("版本: " + getPluginMeta().getVersion());
         getLogger().info("星兮兮!Zcc!");
         //注册数据包监听器
         ProtocolLibrary.getProtocolManager().addPacketListener(new ChatNetworkListener(this, PacketType.Play.Client.CHAT));
