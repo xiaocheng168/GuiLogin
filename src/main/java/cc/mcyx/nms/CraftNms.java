d9726a03851c55d51e342c916c3d228a40cba9ba
Zcc <x3513312668@gmail.com>
2023-09-15 04:04:42 +0800

    NMS 将 CraftNms.serverId 改为了全局变量，方便四面八方调用

--- src/main/java/cc/mcyx/nms/CraftNms.java
+++ src/main/java/cc/mcyx/nms/CraftNms.java
@@ -4,6 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 
 /**
@@ -13,6 +15,19 @@ public abstract class CraftNms {
 
     //解析NMS版本
     public static final String nmsVersion = (Bukkit.getServer()).getClass().getPackage().getName().replace("org.bukkit.craftbukkit.", "");
+    //服务器版本号
+    public static int serverId;
+
+    static {
+        //计算服务器版本号
+        String bukkitVersion = Bukkit.getBukkitVersion();
+        //获取服务器版本号
+        String serverStrId = bukkitVersion.substring(0, bukkitVersion.indexOf("-")).replace(".", "");
+        //预防某些1.17后没有的单独版本，出现这种情况后面补一个0
+        serverId = Integer.parseInt(
+                serverStrId.length() <= 3 ? serverStrId + "0" : serverStrId
+        );
+    }
 
     /**
      * 使用反射获取玩家目前GUI序号
@@ -40,7 +55,20 @@ public abstract class CraftNms {
      */
     public static Object getChatComponentText(String str) {
         try {
-            Class<?> aClass = Class.forName("net.minecraft.server.v1_16_R3.ChatComponentText");
+            Class<?> aClass;
+            if (serverId >= 1190) {
+                //>= 1.19
+                aClass = Class.forName("net.minecraft.network.chat.IChatBaseComponent");
+                Method a = aClass.getMethod("a", String.class);
+                return a.invoke(aClass, str);
+            }
+            if (serverId >= 1180) {
+                //>= 1.17
+                aClass = Class.forName("net.minecraft.network.chat.ChatComponentText");
+            } else {
+                // < 1.17
+                aClass = Class.forName("net.minecraft.server." + nmsVersion + ".ChatComponentText");
+            }
             Constructor<?> constructor = aClass.getConstructor(String.class);
             return constructor.newInstance(str);
         } catch (Exception e) {
@@ -48,8 +76,30 @@ public abstract class CraftNms {
         }
     }
 
+    /**
+     * 获取打开GUI类型
+     * 1.8 - 1.13 string
+     * 1.14-1.16 int
+     * 1.17+ Containers Object
+     *
+     * @return 返回GUI打开方案对象
+     */
+
     public static Object getOpenGuiType() {
-        return 7;
+        //如果版本小于1.13.2以下
+        if (serverId <= 1132) {
+            return "minecraft:anvil";
+        } else if (serverId >= 1170) {
+            //高版本1.17=+
+            try {
+                Class<?> aClass = Class.forName("net.minecraft.world.inventory.Containers");
+                Field h = aClass.getDeclaredField("h");
+                h.setAccessible(true);
+                return h.get(aClass);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else return 8;
     }
 
 }
