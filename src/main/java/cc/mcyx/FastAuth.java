c3647ba3d558f688424086b2bea6c35c75d2e9bd
Zcc <x3513312668@gmail.com>
2023-09-15 01:12:42 +0800

    兼容更多版本是spigot服务端

--- src/main/java/cc/mcyx/FastAuth.java
+++ src/main/java/cc/mcyx/FastAuth.java
@@ -2,17 +2,18 @@ package cc.mcyx;
 
 import cc.mcyx.api.Metrics;
 import cc.mcyx.config.ConfigManager;
+import cc.mcyx.config.MessageSender;
 import cc.mcyx.listener.ChatNetworkListener;
 import cc.mcyx.listener.GuiNetworkListener;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
-import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-public class FastAuth extends JavaPlugin {
+public class FastAuth extends JavaPlugin implements Listener {
 
     public static FastAuth fastAuth;
 
@@ -34,7 +35,7 @@ public class FastAuth extends JavaPlugin {
     public void onEnable() {
         getLogger().info("只为登录更快一步!");
         getLogger().info("FastAuth 已载入");
-        getLogger().info("版本: " + getPluginMeta().getVersion());
+        getLogger().info("版本: " + getDescription().getVersion());
         getLogger().info("星兮兮!Zcc!");
         //注册数据包监听器
         ProtocolLibrary.getProtocolManager().addPacketListener(new ChatNetworkListener(this, PacketType.Play.Client.CHAT));
@@ -44,7 +45,7 @@ public class FastAuth extends JavaPlugin {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         this.reloadConfig();
-        sender.sendMessage(Component.text(ConfigManager.getMessage("reload", "配置文件已刷新!")));
+        MessageSender.sendMessage(sender, ConfigManager.getMessage("reload", "配置文件已刷新!"));
         return true;
     }
 }
